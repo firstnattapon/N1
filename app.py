531385e77491226a8c7bf2cf9fc42c0bf83ca0d6
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-02 00:39:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -116,8 +116,7 @@ class Run_model :
 
 model =  Run_model()
 ex = model.ex_api
-# st.write(pd.DataFrame(ex.private_get_get_position({'instrument_name': 'ETH-PERPETUAL'})['result']))
-print(ex.private_get_get_position({'instrument_name': 'ETH-PERPETUAL'})['result'])
+st.write(ex.private_get_get_position({'instrument_name': 'ETH-PERPETUAL'})['result'])
 
 
 
