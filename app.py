80b61652494dfa85eace990015f0902e91c2adf1
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-08 11:01:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -93,23 +93,25 @@ class Run_model :
                 bar.progress(i + 1)
                 sleep(self.sleep)
 
-model =  Run_model()
-model.pair_trade = st.sidebar.text_input('Symbol' , 'ETH-PERPETUAL')
-model.apiKey = st.sidebar.text_input('apiKey' , "AtdG0K3k")
-model.secret = st.sidebar.text_input('apiKey' ,"lItUXWckP2PNN-uPnrP_h_0dsctCXdFVP9x73bwo3Nc")
-model.start_capital = st.sidebar.slider('start_capital' , 0 , 500 , 225)
-model.sleep = st.sidebar.slider('sleep' , 0.0 , 6.0 , 3.0)
+while True:
+    model =  Run_model()
+    model.pair_trade = st.sidebar.text_input('Symbol' , 'ETH-PERPETUAL')
+    model.apiKey = st.sidebar.text_input('apiKey' , "AtdG0K3k")
+    model.secret = st.sidebar.text_input('apiKey' ,"lItUXWckP2PNN-uPnrP_h_0dsctCXdFVP9x73bwo3Nc")
+    model.start_capital = st.sidebar.slider('start_capital' , 0 , 500 , 225)
+    model.sleep = st.sidebar.slider('sleep' , 0.0 , 6.0 , 3.0)
 
-st.sidebar.text("_"*50)
-st.sidebar.text("start_capital : {}".format (model.start_capital))
-st.sidebar.text("Dense_11 : {}".format (model.Dense_11))
-st.sidebar.text("Dense_12 : {}".format (model.Dense_12))
-st.sidebar.text("Dense_21 : {}".format (model.Dense_21))
-st.sidebar.text("Dense_22 : {}".format (model.Dense_22))
-st.sidebar.text("Dense_31 : {}".format (model.Dense_31))
-st.sidebar.text("Dense_32 : {}".format (model.Dense_32))
-st.sidebar.text("_"*50)
-
-if st.sidebar.button('Run_model'):
-        model =  Run_model()
-        model.trade
+    st.sidebar.text("_"*50)
+    st.sidebar.text("start_capital : {}".format (model.start_capital))
+    st.sidebar.text("Dense_11 : {}".format (model.Dense_11))
+    st.sidebar.text("Dense_12 : {}".format (model.Dense_12))
+    st.sidebar.text("Dense_21 : {}".format (model.Dense_21))
+    st.sidebar.text("Dense_22 : {}".format (model.Dense_22))
+    st.sidebar.text("Dense_31 : {}".format (model.Dense_31))
+    st.sidebar.text("Dense_32 : {}".format (model.Dense_32))
+    st.sidebar.text("_"*50)
+    model.trade
+    
+#   if st.sidebar.button('Run_model'):
+#       model =  Run_model()
+#       model.trade
