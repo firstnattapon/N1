bca28eb74749b9932c8ef08d523ad65df601ae72
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-10-18 02:32:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ filter 	  =  st.sidebar.text_input('filter','T')
 pair 		   = [i for i in e if i[-1] == filter]
 
 coin_beta_expander = st.sidebar.beta_expander('coin')
-  coin = coin_beta_expander.radio('coin',tuple(pair))
+coin = coin_beta_expander.radio('coin',tuple(pair))
   
 timeframe =  st.sidebar.selectbox('time',('1d' , '15m' ,'1h' , '4h'))
 # limit     =   st.sidebar.selectbox('limit',(180 , 270 , 365))
