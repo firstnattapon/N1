3e248fdc650851c45c34ac2f867a4bfe3a921637
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-10-26 01:04:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,16 +18,34 @@ st.beta_set_page_config(
 exchange = ccxt.binance({'apiKey': ''   ,'secret':  ''  , 'enableRateLimit': True }) 
 e = exchange.load_markets()
 filter 	  =  st.sidebar.text_input('filter','T')
-pair   = [i for i in e if i[-1] == filter]
-coin_beta_expander = st.sidebar.beta_expander('coin')
-coin = coin_beta_expander.radio('coin',tuple(pair))
-timeframe =  st.sidebar.selectbox('time',('1d' , '15m' ,'1h' , '4h'))
-limit_a     =   st.sidebar.number_input('limit_a',value=90)
-limit_b     =   st.sidebar.number_input('limit_b',value=180)
-limit_c     =   st.sidebar.number_input('limit_c',value=270)
-limit_d     =   st.sidebar.number_input('limit_d',value=365)
-n_changepoints =  st.sidebar.number_input('n_changepoints',min_value=0,value=25,step=1)
-shift_d   = st.sidebar.number_input('shift_d', 1)
+pair_1   = [i for i in e if i[-1] == filter]
+
+@st.cache(suppress_st_warning=True)
+def z (coin):
+  ohlcv =  exchange.fetch_ohlcv( coin  , '1h' , limit=1000)
+  ohlcv = exchange.convert_ohlcv_to_trading_view(ohlcv)
+  df =  pd.DataFrame(ohlcv)
+  df.t = df.t.apply(lambda  x :  datetime.datetime.fromtimestamp(x)) ; df = df.dropna()
+  Prop = df
+  Prop['ds'] = Prop['t'] 
+  Prop['y'] =  (Prop['o']  + Prop['h']  +Prop['l']  +Prop['c'] ) / 4
+  Prop = Prop.iloc[ : , -2:]
+  return Prop
+
+def sum_all_z (Prop):
+  pct = pd.DataFrame()
+  pct['y'] = Prop.y.pct_change()
+  pct['ohlc'] = Prop.y
+  pct['cf_buy'] =  pct.y.map( lambda  x : np.where (x > 0 , x  , 0 ))  
+  pct['sum_buy'] = pct.cf_buy.cumsum()    
+  pct['cf_sell'] =  pct.y.map( lambda  x : np.where (x < 0 , abs(x)  , 0) )  
+  pct['sum_sell'] = pct.cf_sell.cumsum() 
+  pct['cf_all'] =  pct.y.map( lambda  x : abs(x) )  
+  pct['sum_all'] = pct.cf_all.cumsum() 
+  pct = pct[['sum_buy', 'sum_sell' ,'sum_all']]
+  pct = pct.tail(1)
+  pct = pct.reset_index()
+  return pct
 
 @st.cache(suppress_st_warning=True)
 def A ():
@@ -116,37 +134,6 @@ def D ():
   fig = add_changepoints_to_plot((m.plot(forecast)).gca(), m, forecast)
   st.pyplot() ; #st.write(Prop.tail(1))
   return Prop , forecast
-
-@st.cache(suppress_st_warning=True)
-def z (coin):
-  global shift_d 
-  ohlcv =  exchange.fetch_ohlcv( coin  , '1h' , limit=1000)
-  ohlcv = exchange.convert_ohlcv_to_trading_view(ohlcv)
-  df =  pd.DataFrame(ohlcv)
-  df.t = df.t.apply(lambda  x :  datetime.datetime.fromtimestamp(x)) ; df = df.dropna()
-
-  shift_d = shift_d
-  Prop = df
-  Prop['ds'] = Prop['t'] 
-  Prop['y'] =  (Prop['o']  + Prop['h']  +Prop['l']  +Prop['c'] ) / 4
-  Prop = Prop.iloc[ : , -2:]
-  return Prop
-
-def sum_all_z (Prop):
-  pct = pd.DataFrame()
-  pct['y'] = Prop.y.pct_change()
-  pct['ohlc'] = Prop.y
-  pct['cf_buy'] =  pct.y.map( lambda  x : np.where (x > 0 , x  , 0 ))  
-  pct['sum_buy'] = pct.cf_buy.cumsum()    
-  pct['cf_sell'] =  pct.y.map( lambda  x : np.where (x < 0 , abs(x)  , 0) )  
-  pct['sum_sell'] = pct.cf_sell.cumsum() 
-  pct['cf_all'] =  pct.y.map( lambda  x : abs(x) )  
-  pct['sum_all'] = pct.cf_all.cumsum() 
-  pct = pct[['sum_buy', 'sum_sell' ,'sum_all']]
-  pct = pct.tail(1)
-  pct = pct.reset_index()
-#   st.write(pct.tail(1))  
-  return pct
   
 def sum_all (Prop ,forecast):
   pct = pd.DataFrame()
@@ -168,10 +155,10 @@ col1, col2 = st.beta_columns(2)
 col3, col4 = st.beta_columns(2)
 
 with col0:
-  Prop = z(pair[:1][-1])
+  Prop = z(pair_1[:1][-1])
   df_1 = sum_all_z(Prop)
   df_1['index'] = 'BTC/USDT'
-  for i in pair[1:]:
+  for i in pair_1[1:]:
     Prop = z(i)
     df_2 = sum_all_z(Prop)
     df_2['index'] = i
@@ -180,8 +167,19 @@ with col0:
   st.write(df_1)  
   df_f = df_1.head(20)
   df_f = df_f.index
-  pair = df_f
+  pair_2 = df_f
+
+coin_beta_expander = st.sidebar.beta_expander('coin')
+coin = coin_beta_expander.radio('coin',tuple(pair_2))
+timeframe =  st.sidebar.selectbox('time',('1d' , '15m' ,'1h' , '4h'))
+limit_a     =   st.sidebar.number_input('limit_a',value=90)
+limit_b     =   st.sidebar.number_input('limit_b',value=180)
+limit_c     =   st.sidebar.number_input('limit_c',value=270)
+limit_d     =   st.sidebar.number_input('limit_d',value=365)
+n_changepoints =  st.sidebar.number_input('n_changepoints',min_value=0,value=25,step=1)
+shift_d   = st.sidebar.number_input('shift_d', 1)  
   
+
 with col1:
   Prop , forecast = A()
   col1_expander = st.beta_expander('90' , expanded=True)
