93a4eba206a4658a9ad001bed6cbaf0d43b38087
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-11-01 12:02:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.beta_set_page_config(
 
 # sns.set_style("whitegrid")
 
-exchange = ccxt.binance({'apiKey': ''   ,'secret':  ''  , 'enableRateLimit': True }) 
+exchange = ccxt.okex({'apiKey': ''   ,'secret':  ''  , 'enableRateLimit': True }) 
 e = exchange.load_markets()
 
 filter = st.sidebar.beta_expander('filter')
